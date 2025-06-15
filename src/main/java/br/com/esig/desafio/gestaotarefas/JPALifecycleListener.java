df7685bcc817f228f3453a59f46492dbbd5f1cb9
gloria-mariass <gloria.souza.110@ufrn.edu.br>
2025-06-15 14:01:30 -0300

    fix: Enhance JPALifecycleListener logging and update deadline label in cadastro_tarefas.xhtml

--- src/main/java/br/com/esig/desafio/gestaotarefas/JPALifecycleListener.java
+++ src/main/java/br/com/esig/desafio/gestaotarefas/JPALifecycleListener.java
@@ -1,5 +1,6 @@
 package br.com.esig.desafio.gestaotarefas;
 
+import java.util.logging.Logger;
 import br.com.esig.desafio.gestaotarefas.util.JPAUtil;
 
 import javax.servlet.ServletContextEvent;
@@ -8,13 +9,16 @@ import javax.servlet.annotation.WebListener;
 
 @WebListener
 public class JPALifecycleListener implements ServletContextListener {
+    private static final Logger LOGGER = Logger.getLogger(JPALifecycleListener.class.getName());
 
     @Override
     public void contextInitialized(ServletContextEvent sce) {
+        LOGGER.info("JPALifecycleListener: Aplicação web inicializada. Iniciando JPA.");
     }
 
     @Override
     public void contextDestroyed(ServletContextEvent sce) {
+        LOGGER.info("JPALifecycleListener: Aplicação web sendo encerrada. Fechando JPA.");
         JPAUtil.fecharFactory();
     }
 }
